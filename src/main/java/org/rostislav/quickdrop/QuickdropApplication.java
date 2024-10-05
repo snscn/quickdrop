a4a214bfb8da4b6f9ad552c1157994bf33efde30
Rostislav Raykov <rostislav.raykov@infinno.eu>
2024-10-05 22:26:00 +0300

    Initial commit

---
+++ src/main/java/org/rostislav/quickdrop/QuickdropApplication.java
@@ -0,0 +1,13 @@
+package org.rostislav.quickdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickdropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickdropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rostislav/quickdrop/QuickdropApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rostislav.quickdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickdropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
